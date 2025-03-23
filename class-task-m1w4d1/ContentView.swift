4b61e75a44455756e9140e8e1c2af77d9adbaf28
Naif Ghannam <dhdfaui@gmail.com>
2025-03-23 13:15:58 +0300

    Initial Commit

---
+++ class-task-m1w4d1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  class-task-m1w4d1
+//
+//  Created by Mac on 23/09/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ class-task-m1w4d1/class_task_m1w4d1App.swift
@@ -0,0 +1,17 @@
+//
+//  class_task_m1w4d1App.swift
+//  class-task-m1w4d1
+//
+//  Created by Mac on 23/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct class_task_m1w4d1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
